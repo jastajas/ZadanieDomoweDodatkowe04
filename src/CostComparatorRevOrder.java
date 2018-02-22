4bbb8e0b7e7d5bb7c520805a30bffec3960fe019
jackS <jastajas@gamil.com>
2018-02-22 20:59:22 +0100

    Comparator cost (rev order) - ver 1

---
+++ src/CostComparatorRevOrder.java
@@ -0,0 +1,13 @@
+import java.util.Comparator;
+
+public class CostComparatorRevOrder implements Comparator<Expenditure> {
+    @Override
+    public int compare(Expenditure o1, Expenditure o2) {
+        if(o1.getCost() > o2.getCost())
+            return -1;
+        else if(o1.getCost() == o2.getCost())
+            return 0;
+        else
+            return 1;
+    }
+}
